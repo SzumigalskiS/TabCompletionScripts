d484335e75327940612afe6bf10720f32fdb7239
Sebastian <Sebastian@ohayo.local>
2017-03-25 20:51:23 +0800

    EventInstanceID

---
+++ EventInstanceID.ps1
@@ -0,0 +1,9 @@
+﻿$EventID = {
+    param($CommandName, $ParameterName, $WordToComplete)
+
+    $Instances = Import-Csv D:\PSTesting\Events.csv
+    foreach($Instance in $Instances){
+        New-CompletionResult -CompletionText $Instance.EventID -ToolTip $Instance.Message -ListItemText $Instance.EventID
+    }
+}
+Register-ArgumentCompleter -CommandName @( 'Get-EventLog' ) -ParameterName InstanceID -ScriptBlock $EventID
